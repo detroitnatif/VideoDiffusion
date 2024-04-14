150a3518429d184730c8def2bfb7ce527f9f81c4
detroitnatif <tytydizzle124@gmail.com>
2024-04-14 22:21:29 +0200

    creating residual decoder block

--- sd/attention.py
+++ sd/attention.py
@@ -3,7 +3,36 @@ from torch import nn
 from torch.nn import functional as F
 from decoder import VAE_AttentionBlock, VAE_ResidualBlock
 from sd.attention import SelfAttention
+import math
 
 
 class SelfAttention(nn.Module):
-    
+    def __init__(self, n_heads, d_embed, in_proj_bias=True, out_proj_bias=True):
+        super().__init__()
+
+        self.in_proj = nn.Linear(d_embed, 3 * d_embed, bias=in_proj_bias)
+
+        self.out_proj = nn.Linear(d_embed, d_embed, bias=in_proj_bias)
+        self.n_heads = n_heads
+        self.d_head = d_embed // n_heads
+
+    def forward(self, x, casual_mask=False):
+        # x.shape = batch_size, sequence length, dimension
+        input_shape = x.shape
+
+        batch_size, sequence_length, d_embed = input_shape
+
+        intermediate_shape = (batch_size, sequence_length, self.n_heads, self.d_head)
+
+        k, v, q = self.in_proj(x).chunk(3, dim=-1)
+
+        q = q.view(intermediate_shape).transpose(1, 2)
+        k = k.view(intermediate_shape).transpose(1, 2)
+        v = v.view(intermediate_shape).transpose(1, 2)
+
+        weights = q @ k.transpose(-1, 2)
+
+        if casual_mask:
+            mask = torch.ones_like(weights, dtype=torch.bool).triu(1)
+            weights.masked_fill(mask, -torch.inf)
+        weight /= math.sqrt(self.d_head)
